09e33673d5e04b48dbecad172869a8182e28a15e
mrasilbek19 <asilbek190303@gmail.com>
2026-07-02 17:45:44 +0900

    fix: lib folder addition

--- src/app/screens/helpPage/index.tsx
+++ src/app/screens/helpPage/index.tsx
@@ -14,115 +14,115 @@ import { faq } from "../../../lib/data/faq";
 import { terms } from "../../../lib/data/terms";
 
 export default function HelpPage() {
-  const [value, setValue] = React.useState("1");
+    const [value, setValue] = React.useState("1");
 
-  /** HANDLERS **/
-  
-  const handleChange = (e: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
+    /** HANDLERS **/
 
-  return (
-    <div className={"help-page"}>
-      <Container className={"help-container"}>
-        <TabContext value={value}>
-          <Box className={"help-menu"}>
-            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="lab API tabs example"
-                className={"table_list"}
-              >
-                <Tab label="TERMS" value={"1"} />
-                <Tab label="FAQ" value={"2"} />
-                <Tab label="CONTACT" value={"3"} />
-              </Tabs>
-            </Box>
-          </Box>
-          <Stack>
-            <Stack className={"help-main-content"}>
-              <TabPanel value={"1"}>
-                <Stack className={"rules-box"}>
-                  <Box className={"rules-frame"}>
-                    {terms.map((value, number) => {
-                      return <p key={number}>{value}</p>;
-                    })}
-                  </Box>
-                </Stack>
-              </TabPanel>
-              <TabPanel value={"2"}>
-                <Stack className={"accordion-menu"}>
-                  {faq.map((value, number) => {
-                    return (
-                      <Accordion key={number}>
-                        <AccordionSummary
-                          expandIcon={<ExpandMoreIcon />}
-                          aria-controls="panel1a-content"
-                          id="panel1a-header"
-                        >
-                          <Typography>{value.question}</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                          <Typography>{value.answer}</Typography>
-                        </AccordionDetails>
-                      </Accordion>
-                    );
-                  })}
-                </Stack>
-              </TabPanel>
-              <TabPanel value={"3"}>
-                <Stack className={"admin-letter-box"}>
-                  <Stack className={"admin-letter-container"}>
-                    <Box className={"admin-letter-frame"}>
-                      <span>Contact us!</span>
-                      <p>Fill out below form to send a message!</p>
+    const handleChange = (e: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={"help-page"}>
+            <Container className={"help-container"}>
+                <TabContext value={value}>
+                    <Box className={"help-menu"}>
+                        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                            <Tabs
+                                value={value}
+                                onChange={handleChange}
+                                aria-label="lab API tabs example"
+                                className={"table_list"}
+                            >
+                                <Tab label="TERMS" value={"1"} />
+                                <Tab label="FAQ" value={"2"} />
+                                <Tab label="CONTACT" value={"3"} />
+                            </Tabs>
+                        </Box>
                     </Box>
-                    <form
-                      action={"#"}
-                      method={"POST"}
-                      className={"admin-letter-frame"}
-                    >
-                      <div className={"admin-input-box"}>
-                        <label>Your name</label>
-                        <input
-                          type={"text"}
-                          name={"memberNick"}
-                          placeholder={"Type your name here"}
-                        />
-                      </div>
-                      <div className={"admin-input-box"}>
-                        <label>Your email</label>
-                        <input
-                          type={"text"}
-                          name={"memberEmail"}
-                          placeholder={"Type your email here"}
-                        />
-                      </div>
-                      <div className={"admin-input-box"}>
-                        <label>Message</label>
-                        <textarea
-                          name={"memberMsg"}
-                          placeholder={"Your message"}
-                        ></textarea>
-                      </div>
-                      <Box
-                        display={"flex"}
-                        justifyContent={"flex-end"}
-                        sx={{ mt: "30px" }}
-                      >
-                        <Button type={"submit"} variant="contained">
-                          Send
-                        </Button>
-                      </Box>
-                    </form>
-                  </Stack>
-                </Stack>
-              </TabPanel>
-            </Stack>
-          </Stack>
-        </TabContext>
-      </Container>
-    </div>
-  );
+                    <Stack>
+                        <Stack className={"help-main-content"}>
+                            <TabPanel value={"1"}>
+                                <Stack className={"rules-box"}>
+                                    <Box className={"rules-frame"}>
+                                        {terms.map((value, number) => {
+                                            return <p key={number}>{value}</p>;
+                                        })}
+                                    </Box>
+                                </Stack>
+                            </TabPanel>
+                            <TabPanel value={"2"}>
+                                <Stack className={"accordion-menu"}>
+                                    {faq.map((value, number) => {
+                                        return (
+                                            <Accordion key={number}>
+                                                <AccordionSummary
+                                                    expandIcon={<ExpandMoreIcon />}
+                                                    aria-controls="panel1a-content"
+                                                    id="panel1a-header"
+                                                >
+                                                    <Typography>{value.question}</Typography>
+                                                </AccordionSummary>
+                                                <AccordionDetails>
+                                                    <Typography>{value.answer}</Typography>
+                                                </AccordionDetails>
+                                            </Accordion>
+                                        );
+                                    })}
+                                </Stack>
+                            </TabPanel>
+                            <TabPanel value={"3"}>
+                                <Stack className={"admin-letter-box"}>
+                                    <Stack className={"admin-letter-container"}>
+                                        <Box className={"admin-letter-frame"}>
+                                            <span>Contact us!</span>
+                                            <p>Fill out below form to send a message!</p>
+                                        </Box>
+                                        <form
+                                            action={"#"}
+                                            method={"POST"}
+                                            className={"admin-letter-frame"}
+                                        >
+                                            <div className={"admin-input-box"}>
+                                                <label>Your name</label>
+                                                <input
+                                                    type={"text"}
+                                                    name={"memberNick"}
+                                                    placeholder={"Type your name here"}
+                                                />
+                                            </div>
+                                            <div className={"admin-input-box"}>
+                                                <label>Your email</label>
+                                                <input
+                                                    type={"text"}
+                                                    name={"memberEmail"}
+                                                    placeholder={"Type your email here"}
+                                                />
+                                            </div>
+                                            <div className={"admin-input-box"}>
+                                                <label>Message</label>
+                                                <textarea
+                                                    name={"memberMsg"}
+                                                    placeholder={"Your message"}
+                                                ></textarea>
+                                            </div>
+                                            <Box
+                                                display={"flex"}
+                                                justifyContent={"flex-end"}
+                                                sx={{ mt: "30px" }}
+                                            >
+                                                <Button type={"submit"} variant="contained">
+                                                    Send
+                                                </Button>
+                                            </Box>
+                                        </form>
+                                    </Stack>
+                                </Stack>
+                            </TabPanel>
+                        </Stack>
+                    </Stack>
+                </TabContext>
+            </Container>
+        </div>
+    );
 }
