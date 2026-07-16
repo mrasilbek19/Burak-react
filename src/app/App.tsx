311b019fe5b0ede0679cc900a01ec89ccd3fc02d
mrasilbek19 <asilbek190303@gmail.com>
2026-07-16 17:53:51 +0900

    fix: modify homePage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -36,8 +36,8 @@ function App() {
           <HelpPage />
         </Route>
         <Route path="/">
-          <Test />
-          {/* <HomePage /> */}
+          {/* <Test /> */}
+          <HomePage />
         </Route>
       </Switch>
       <Footer />
