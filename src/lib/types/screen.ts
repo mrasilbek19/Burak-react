212a502107949c1865dc20466dc23811aa1279b4
mrasilbek19 <asilbek190303@gmail.com>
2026-07-18 13:37:23 +0900

    feat: develop AppRootState and HomePage type integration

---
+++ src/lib/types/screen.ts
@@ -0,0 +1,23 @@
+import { Member } from "./member";
+import { Product } from "./product";
+
+
+/** REACT APP STATE **/
+export interface AppRootState {
+    homePage: HomePageState;
+}
+
+/** HomePage **/
+export interface HomePageState {
+    popularDishes: Product[];
+    newDishes: Product[];
+    topUsers: Member[]
+}
+
+
+
+/** Products Page **/
+
+
+
+/** Orders Page **/
